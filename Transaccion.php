f039bd323dc8622fa3ff59270f7cb67a44b5ebcc
alberto <alberto.vs@outlook.com>
2020-12-11 15:39:31 -0500

    11122020-1524

--- Transaccion.php
+++ Transaccion.php
@@ -10,6 +10,16 @@ MercadoPago\SDK::setAccessToken('TEST-6213750683445204-121120-2e904ad2a41705a5a8
 //PASO 3
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
+
+//URL DE RETORNO AL FINALIZAR TRANSACCION
+$preference->back_urls = array(
+    "success" => "http://localhost:82/mp-ecommerce-php/",
+    "failure" => "http://http://localhost:82/mp-ecommerce-php/rechazado.php?error=failure",
+    "pending" => "http://localhost:82/mp-ecommerce-php/pendiente.php?error=pending"
+);
+$preference->auto_return = "approved";
+
+
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
 $item->title = 'Mi producto';
@@ -29,7 +39,7 @@ $preference->save();
     <title>Document</title>
 </head>
 <body>
-    <form action="Confirmacion.php" method = "POST">
+    <form action="confirmacion.php" method = "POST">
     <script
         src="https://www.mercadopago.com.pe/integrations/v1/web-payment-checkout.js"
         data-preference-id="<?php echo $preference->id; ?>">
